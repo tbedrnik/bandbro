5db8fdbeafa045fbd0feca3d8447407b0cbe6b01
Tomas Bedrnik <bedrniktom@gmail.com>
2026-01-02 03:35:23 +0100

    temp db ep

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -7,5 +7,6 @@ export const api = new Elysia({prefix: '/api'})
 .use(serverTiming())
 .use(authMiddleware)
 .get('/add', () => addNumbers(1, 2))
+.get('/db', () => Bun.file((process.env.DATABASE_URL || "").replace('file:', '')))
 
 export type Api = typeof api;
