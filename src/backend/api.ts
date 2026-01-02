1ce9977b3b3889b3402b51d2308c57a5c691c687
Tomas Bedrnik <bedrniktom@gmail.com>
2026-01-02 03:48:41 +0100

    temp db ep 2

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -3,10 +3,15 @@ import { addNumbers } from "../shared/addNumbers";
 import { authMiddleware } from "./auth";
 import {serverTiming} from "@elysiajs/server-timing";
 
+const dbPath = (process.env.DATABASE_URL || "").replace('file:', '')
+const dbFile = Bun.file(dbPath)
+
 export const api = new Elysia({prefix: '/api'})
 .use(serverTiming())
 .use(authMiddleware)
 .get('/add', () => addNumbers(1, 2))
-.get('/db', () => Bun.file((process.env.DATABASE_URL || "").replace('file:', '')))
+.get('/db/get', () => dbFile)
+.get('/db/meta', () => dbFile.exists().then((aha) => `db file "${dbPath}" exists: ${aha}`))
+.get('/file', () => Bun.file(new URL(import.meta.url)))
 
 export type Api = typeof api;
